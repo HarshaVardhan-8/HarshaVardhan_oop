0b3d5833d6e577511592af25511280fae7abde20
HI <HI@DESKTOP-3KEP4FF.mshome.net>
2020-03-03 10:37:53 +0530

    Initial

---
+++ oops/src/main/java/com/maven/oops/wraffers.java
@@ -0,0 +1,11 @@
+package com.maven.oops;
+
+public class wraffers extends Sweets {
+    String name;
+    int weight;
+    int cost;
+    String color;
+    wraffers(String name, int weight, int cost, String color){
+        super(name, weight, cost, color);
+    }
+}
